f54e561ab5b21bd0e06fe044e1fb7afb0dcb4a77
M. Scott Ford <vaderpi@nypoint.newyawk.us>
2009-08-30 15:09:20 -0400

    Added check for the div that the create scenario form fields are in.

--- features/step_definitions/scenarios_steps.rb
+++ features/step_definitions/scenarios_steps.rb
@@ -11,23 +11,29 @@ When /^add scenario is clicked$/ do
 end
 
 When /^"([^\"]*)" is typed as the feature name$/ do |feature_name|
-  fill_in "feature_name", :with => feature_name
+  within "#create_scenario" do 
+    fill_in "feature_name", :with => feature_name
+  end
 end
 
 When /^"([^\"]*)" is typed as the scenario name$/ do |scenario_name|
-  fill_in "scenario_name", :with => scenario_name
+  within "#create_scenario" do
+    fill_in "scenario_name", :with => scenario_name
+  end
 end
 
 When /^the scenario body is typed as$/ do |scenario_body|
-  fill_in "scenario_body", :with => scenario_body
+  within "#create_scenario" do
+    fill_in "scenario_body", :with => scenario_body
+  end
 end
 
 When /^create scenario is clicked$/ do
-  click_link "Create Scenario"
+  click_link_within "#create_scenario", "Create Scenario"
 end
 
 Then /^the feature list should contain a feature named "([^\"]*)"$/ do |arg1|
-  pending
+  pending  
 end
 
 Then /^the feature "([^\"]*)" should have a scenario named "([^\"]*)" with body$/ do |arg1, arg2, string|
