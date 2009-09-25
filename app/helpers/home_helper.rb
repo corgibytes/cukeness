c1cf6b7a17e02e16214240aa463543799cac3660
M. Scott Ford <scott@vaderpi.com>
2009-09-24 21:14:25 -0400

    Got the helper test passing.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,18 +1,18 @@
 module HomeHelper
   def parse_cucumber_output(output)
     result = "<div id=\"features\">\n  <h3>Features:</h3>\n  <div id=\"feature_list\">\n"
+    body_opened = false
     output.each_line do |line|
-      body_opened = false
       if line.include? "Feature:"
         if body_opened 
-          result << "    </div>"
+          result << "    </div>\n"
           body_opened = false
         end
         line = line.gsub("Feature:", "").strip
         result << "    <h4>#{line}</h4>\n"
       elsif line.include? "Scenario:"
         if body_opened 
-          result << "    </div>"
+          result << "    </div>\n"
           body_opened = false
         end
         line = line.gsub("Scenario:", "").strip
@@ -22,7 +22,11 @@ module HomeHelper
         result << "      <span class=\"step undefined\">#{line.strip}</span>\n"
       end
     end
-    result << "  </div>\n</div>"
+    if body_opened 
+      result << "    </div>\n"
+      body_opened = false
+    end
+    result << "  </div>\n</div>\n"
     result
   end
 end
