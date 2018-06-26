d6e05ff26cf913923437f3e63fda13a705158946
Arlo Belshee <arlo.belshee@live.com>
2018-06-26 08:59:55 -0700

    t    Create a test project with one passing test.

---
+++ Cukeness/Cukeness.Test/TheTestsRun.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Cukeness.Test
+{
+	[TestFixture]
+	public class TheTestsRun
+	{
+		[Test]
+		public void TestThatPasses()
+		{
+			1.Should().BeGreaterThan(0);
+		}
+	}
+}
