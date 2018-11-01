1f105fb6ddced57a9938539e5047a0bb19dd6f77
M. Scott Ford <scott@corgibytes.com>
2018-11-01 14:12:50 -0400

    F   IDE Integration and solution setup and added .NET Core version of SpecFlow

---
+++ step-server/Cukeness.Specs/StepDefinitions/GitSteps.cs
@@ -0,0 +1,23 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Cukeness.Specs.StepDefinitions 
+{
+    [Binding]
+    public class GitSteps 
+    {
+        //private readonly CukenessDriver _driver;
+
+        //public GitSteps(CukenessDriver driver) 
+        //{
+        //    _driver = driver ?? throw new ArgumentNullException(nameof(driver));
+        //}
+
+        [When(@"there is an organization named ""([^\""]*)"" with a project named ""([^\""]*)""")]   
+        public void WhenThereIsANewOrganizationNamedWithAProjectNamed(
+            string organizationName, string projectName) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
