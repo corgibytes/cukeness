57eadc3a36540c77edd2862736f2d5d858c47518
M. Scott Ford <scott@corgibytes.com>
2018-11-03 15:10:25 -0400

    a   Whitespace removal

--- step-server/StepServer/StepCommandFactory.cs
+++ step-server/StepServer/StepCommandFactory.cs
@@ -6,19 +6,19 @@ namespace StepServer
   public class StepCommandFactory
   {
     private Assembly _stepsAssembly;
-    
+
     public StepCommandFactory(Assembly stepsStepsAssembly)
     {
       _stepsAssembly = stepsStepsAssembly;
     }
-    
+
     public IStepCommand Create(string message)
     {
       IStepCommand result = null;
-      
+
       var parsedMessage = JArray.Parse(message);
       var command = parsedMessage[0].Value<string>();
-      
+
       if (command == "step_matches")
       {
         result = new StepMatchesCommand(
