db4c2e413de04464840dde1d165daf59a59d7767
M. Scott Ford <scott@corgibytes.com>
2018-11-03 14:22:11 -0400

    c   Removes commented out driver

--- step-server/Cukeness.Specs/StepDefinitions/GitSteps.cs
+++ step-server/Cukeness.Specs/StepDefinitions/GitSteps.cs
@@ -6,13 +6,6 @@ namespace Cukeness.Specs.StepDefinitions
     [Binding]
     public class GitSteps
     {
-        //private readonly CukenessDriver _driver;
-
-        //public GitSteps(CukenessDriver driver) 
-        //{
-        //    _driver = driver ?? throw new ArgumentNullException(nameof(driver));
-        //}
-
         [When(@"there is an organization named ""([^\""]*)"" with a project named ""([^\""]*)""")]   
         public void WhenThereIsANewOrganizationNamedWithAProjectNamed(
             string organizationName, string projectName) 
