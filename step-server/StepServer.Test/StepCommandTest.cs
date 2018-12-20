39eb3f25ddf88a8e17a6d5354806d8e4742b8d1b
M. Scott Ford <scott@corgibytes.com>
2018-12-20 14:31:45 -0500

    c   Corrected URL for wire protocol docs

--- step-server/StepServer.Test/StepCommandTest.cs
+++ step-server/StepServer.Test/StepCommandTest.cs
@@ -8,7 +8,7 @@ using FluentAssertions;
 using Newtonsoft.Json;
 
 // Reference material for this set of tests and what they should be
-// able to handle can be found: https://github.com/cucumber/cucumber-ruby-wire/blob/v0.0.1/features/invoke_message.feature
+// able to handle can be found: https://github.com/cucumber/cucumber-ruby-wire/blob/v1.0.0/features/invoke_message.feature
 
 namespace StepServer.Test
 {
